b5dd75c8e889dfabed3e3b6c9b7998426132bf9b
Ahmed Adel <80907822+ahmed45adel@users.noreply.github.com>
2021-07-19 14:39:18 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+### (FWD) - Web Development Professional Track
+## Weather Journal App
+
+## Table of Contents
+About
+Instructions
+Technologies
+Files
+Approach
+Credits
+Author
+
+## About
+This project makes you know the weather based on the zipcode
+
+## Instructions
+To use this project run 'node server.js 3000' 
+insert zip code 
+insert country code which is two letters 
+add how you're feeling
+
+## Technologies
+This project uses html, css and vanilla javascript for the front end
+Express.js for backend which is based on node.js so you have to have both installed 
+
+## Files
+
+```
+commentsOnlyJS
+website
+- app.js
+- index.html
+- style.css    
+README.md
+server.js
+```
+
+## Approach
+I made the project by dividing it into smaller tasks
+and solved each problem which made it easier
+
+## Credits
+Udacity
+FWD
+
+## Author
+Ahmed Adel
+
---
+++ server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+const posts = [];
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup the Server
+const port = 3000;
+
+const server = app.listen(port, runMyServer);
+
+function runMyServer() {
+    console.log(`running server at port ${port}`)
+}
+// GET
+app.get('/projectData', (req, res) => {
+    res.send(projectData)
+    console.log(projectData);
+});
+// POST
+app.post('/projectData', (req, res) => {
+    console.log(req.body);
+    projectData = req.body;
+    posts.push(req.body);
+    console.log(posts);
+});
+
